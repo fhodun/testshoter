2ca4e77ed10eac1c17d5af36883986ab608665ad
gbaranski <gbaranski19@gmail.com>
2020-12-20 11:42:53 +0100

    feat: add URL validation

--- src/misc.ts
+++ src/misc.ts
@@ -9,3 +9,35 @@ export const checkNewestVersion = async (): Promise<string> => {
   if (!version) throw new Error('Unable to get newest version');
   return json.tag_name;
 };
+
+export const validateTestURL = (
+  strURL: string,
+): { url: URL; err?: undefined } | { err: Error } => {
+  let url;
+  try {
+    url = new URL(strURL);
+    if (url.host !== 'www.testportal.pl')
+      throw new Error('Invalid test host, expected www.testportal.pl');
+    if (url.hostname !== 'www.testportal.pl')
+      throw new Error('Invalid test hostname, expected www.testportal.pl');
+    if (url.origin !== 'https://www.testportal.pl')
+      throw new Error(
+        'Invalid test origin, expected https://www.testportal.pl',
+      );
+    if (url.pathname !== '/exam/LoadTestStart.html')
+      throw new Error(
+        'Invalid test pathname, expected /exam/LoadTestStart.html',
+      );
+    if (url.protocol !== 'https:')
+      throw new Error('Invalid test protocol, expected https:');
+    if (!url.search.startsWith('?t=') || url.search.length !== 15)
+      throw new Error('Invalid test search params, expected ?t=xxxxxxxxxxxx');
+  } catch (err) {
+    return {
+      err,
+    };
+  }
+  return {
+    url,
+  };
+};
