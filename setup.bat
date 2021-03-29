26380463a7e5eaeef160bcad6fa63f57fe257697
Filip Hodun <filiphodun@gmail.com>
2021-03-29 14:53:45 +0200

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set /p token="Enter discord bot token: "
+set /p prefix="Enter prefix: "
+echo DISCORD_TOKEN="%token%" > .env
+echo DISCORD_PREFIX="%prefix%" >> .env
+
+call npm install
+call npm start
+
+pause
