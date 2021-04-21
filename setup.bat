708f9072789ae97a4a3ee87703bbeb667cba2dd8
canicjusz <janmichalak@int.pl>
2021-04-21 10:08:24 +0200

    fixed bat

--- setup.bat
+++ setup.bat
@@ -2,8 +2,8 @@
 
 set /p token="Enter discord bot token: "
 set /p prefix="Enter prefix: "
-echo DISCORD_TOKEN="%token%" > .env
-echo DISCORD_PREFIX="%prefix%" >> .env
+echo DISCORD_TOKEN=%token% > .env
+echo COMMAND_PREFIX=^%prefix% >> .env
 
 call npm install
 call npm start
